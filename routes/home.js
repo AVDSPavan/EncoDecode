6d8af7a7a67b8e49e3104ba7961c8fecbbbcae06
AVDSPavan <saipavan.ala@gmail.com>
2020-08-19 09:36:06 +0530

    manage null

--- routes/home.js
+++ routes/home.js
@@ -1,65 +1,104 @@
 const express = require("express");
 const router = express.Router();
 
-let chartoNum = {};
-for (let i = 0; i < 26; i++) {
-	//let bin= i.toString(2).padStart(5,'0')
-	let ch = String.fromCharCode(97 + i);
-	chartoNum[ch] = i;
+let chartoNum = {}
+let numtoChar = {}
+for(let i=0;i<26;i++){
+	let ch = String.fromCharCode(97+i)
+	chartoNum[ch]=i;
+	numtoChar[i]=ch;
 }
-for (let i = 26; i < 52; i++) {
-	//let bin= i.toString(2).padStart(5,'0')
-	//let ch = parseInt(String.fromCharCode(48+i-27))
-	let ch = String.fromCharCode(65 + i - 26);
-	chartoNum[ch] = i;
+for(let i = 26;i<52;i++){
+	let ch = String.fromCharCode(65+i-26)
+	chartoNum[ch]=i;
+	numtoChar[i]=ch;
 }
 
-s1 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//Base 26 to base 10
 const encode = (str) => {
+	let encoded = [];
+	let res = "";
 	let len = str.length;
-	let result = "";
-	result += str.charCodeAt(len - 1) - 97;
-	for (let i = len - 2; i >= 0; i--)
-		result = result * 26 + (str.charCodeAt(i) - 97);
-	console.log(result);
-	return encode1(result);
-};
-
-//Base 10 to base 52
-const encode1 = (str) => {
-	let result = "";
-	while (str > 0) {
-		let r = str % 52;
-		result += s1[r];
-		str = Math.floor(str / 52);
+	for (let i = 0; i < len; i++) {
+		encoded.push(chartoNum[str[i]].toString(2).padStart(6, "0"));
 	}
-	return result.split("").reverse().join("");
+	len = encoded.length;
+	emptybytes = 8 - (len % 8);
+	for (let i = 0; i < emptybytes; i++) {
+		encoded.push("xxxxxxxx");
+	}
+	len = encoded.length;
+	encoded = encoded.join("");
+	//console.log(encoded)
+	len = encoded.length;
+	numofchunks = parseInt(len / 8);
+	chunks = [];
+	let j = 0;
+	for (let i = 0; i < numofchunks; i++) {
+		chunks.push(encoded.substring(j, j + 8));
+		j += 8;
+	}
+	len = chunks.length;
+	for (let i = 0; i < len; i++) {
+		if (
+			chunks[i][0] !== "x" &&
+			(chunks[i][0] === "0" || chunks[i][0] === "1")
+		) {
+			if (chunks[i][7] === "x") {
+				let z = chunks[i];
+				let x = z.indexOf("x");
+				z = z.substring(0, x);
+				x = 8 - x;
+				let temparr = "";
+				for (let idx = 0; idx < x; idx++) {
+					temparr += "0";
+				}
+				z += temparr;
+				chunks[i] = z;
+			}
+			if (parseInt(chunks[i], 2) === 0) {
+				res += "©";
+			} else {
+				res += String.fromCharCode(parseInt(chunks[i], 2));
+			}
+		}
+	}
+	console.log(chunks);
+	console.log(res);
+	return res;
 };
 
-//Base 56 to Base 10
 const decode = (str) => {
+	let temparr = [];
 	let len = str.length;
-	let result = s1.indexOf(str[len - 1]);
-	for (let i = len - 2; i >= 0; i--) {
-		result = result * 52 + s1.indexOf(str[i]);
+	for (let i = 0; i < len; i++) {
+		if (str[i] === "©") {
+			let temp = 0;
+			temparr.push(temp.toString(2).padStart(8, "0"));
+		} else {
+			temparr.push(str.charCodeAt(i).toString(2).padStart(8, "0"));
+		}
+	}
+	console.log(temparr);
+	temparr = temparr.join("");
+	len = temparr.length;
+	let decoded = [];
+	i = 0;
+	while (i < len) {
+		decoded.push(temparr.substring(i, i + 6));
+		i += 6;
+	}
+	console.log(decoded);
+	len = decoded.length;
+	let result = "";
+	for (let i = 0; i < len - 1; i++) {
+		result += numtoChar[parseInt(decoded[i], 2)];
 	}
 	console.log(result);
-	return decode1(result);
-};
-//Base 10 to Base 26
-const decode1 = (str) => {
-	let result = "";
-	while (str > 0) {
-		let r = str % 26;
-		result += s1[r];
-		str = Math.floor(str / 26);
-	}
-	return result.split("").reverse().join("");
+	return result;
 };
 
-// let base26 = 'abcdefghijklmnopqrstuvwxyz'
-// let base62 = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+let base26 = "abcdefghijklmnopqrstuvwxyz";
+let base64 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+-";
 
 router.post("/", (req, res) => {
 	let originalString = req.body.name;
